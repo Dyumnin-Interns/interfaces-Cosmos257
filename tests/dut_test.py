0272f8b3581341e2159baa5437879d70aa674522
Cosmos257 <roopapramodan@gmail.com>
2025-07-10 21:44:33 +0400

    Update dut_test.py

--- tests/dut_test.py
+++ tests/dut_test.py
@@ -2,44 +2,47 @@ import cocotb
 from cocotb.triggers import Timer, RisingEdge, ReadOnly, NextTimeStep
 from cocotb_bus.drivers import BusDriver
 
-def sb_fun(actual_value):
+def sb_fn(actual_value):
     global expected_value
     assert actual_value==expected_value.pop(0),"Scoreboard matching failed"
     
-class InputDriver(BusDriver):
-    _signals=["rdy", "en", "data"]
+class WriteDriver(BusDriver):
+    _signals=["rdy", "en", "data", "address"]
     def __init__(self,dut,name,clk):
         BusDriver.__init__(self,dut,name,clk)
         self.clk=clk
         self.bus.en.value=0
-        async def driver_send(self,value,sync=True):
+    async def driver_send(self,value,address,sync=True):
+        if self.bus.rdy.value!=1:
+            await RisingEdge(self.bus.rdy)
+        self.bus.en.value=1
+        self.bus.data.value=value
+        self.bus.address.value=address
+        await ReadOnly()
+        await RisingEdge(self.clk)
+        self.bus.en.value=0
+        await NextTimeStep()  
+    
+class ReadDriver(BusDriver):
+    _signals=["rdy", "en", "data", "address"]
+    def __init__(self,dut,name,clk,sb_callback):
+        BusDriver.__init__(self,dut,name,clk)
+        self.clk=clk
+        self.bus.en.value=0
+        self.callback=sb_callback
+        self.append(0)
+    async def driver_send(self,address,sync=True):
+        while True:
             if self.bus.rdy.value!=1:
                 await RisingEdge(self.bus.rdy)
             self.bus.en.value=1
-            self.bus.data.value=value
             await ReadOnly()
+            data_val=self.bus.data.value
+            self.callback(data_val)
             await RisingEdge(self.clk)
-            self.bus.en=0
+            self.bus.en.value=0
             await NextTimeStep()
-    
-class OutputDriver(BusDriver):
-    _signals=["rdy", "en", "data"]
-    def __init__(self,dut,name,clk):
-        BusDriver.__init__(self,dut,name,clk)
-        self.clk=clk
-        self.bus.en.value=0
-        self.callback=sb.callback
-        self.append(0)
-        async def driver_send(self,value,sync=True):
-            while True:
-                if self.bus.rdy.value!=1:
-                    await RisingEdge(self.bus.rdy)
-                self.bus.en.value=1
-                await ReadOnly()
-                self.callback(self.bus.data.value)
-                await RisingEdge(self.clk)
-                self.bus.en=0
-                await NextTimeStep()
+ReadDriver(dut,"read", dut.CLK, callback=sb_fn)
 @cocotb.test()
 async def dut_test(dut):
     global expected_value
