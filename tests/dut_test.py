25d4d51c4c142332b0c968fea1e1f7711f2687b4
Cosmos257 <roopapramodan@gmail.com>
2025-07-10 11:54:13 +0400

    Update dut_test.py

--- tests/dut_test.py
+++ tests/dut_test.py
@@ -1,5 +1,9 @@
 import cocotb
-from cocotb.triggers import Timer
+from cocotb.triggers import Timer, RisingEdge, ReadOnly, NextTimeStep
+from cocotb_bus.drivers import BusDriver
+from cocotb.bus.monitors import BusMonitor
+from cocotb_coverage.coverage import CoverCross, CoverPoint, coverage_db
+import os
 
 
 @cocotb.test()
